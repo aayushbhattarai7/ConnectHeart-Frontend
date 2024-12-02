df7e653696a812422c3890defe0e7020133f15ad
Aayush Bhattarai <aayush.bhattarai.np@gmail.com>
2024-12-02 22:03:39 +0545

    implemented socket

--- src/ui/landingPage/organisms/Socket.tsx
+++ src/ui/landingPage/organisms/Socket.tsx
@@ -18,3 +18,25 @@
 // };
 
 // export default Socket;
+
+
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
+
+const Socket = () => {
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log("socket connected", socket.id);
+        });
+
+        return () => {
+            socket.off('connect');
+        };
+    }, []);
+
+    return null;
+};
+
+export default Socket;
